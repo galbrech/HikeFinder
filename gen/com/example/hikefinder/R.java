0b6f2211468bd45dc6070feffc72d947db26d9ae
HoopRockStar <jlparris@ucsc.edu>
2014-02-17 14:16:30 -0800

    partial completion of selection screen

--- gen/com/example/hikefinder/R.java
+++ gen/com/example/hikefinder/R.java
@@ -29,11 +29,22 @@ public final class R {
         public static final int yosemite=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
+        public static final int action_settings=0x7f09000f;
         public static final int button1=0x7f090000;
         public static final int button2=0x7f090001;
-        public static final int checkBox1=0x7f090002;
-        public static final int checkedTextView1=0x7f090003;
+        public static final int checkBox1=0x7f090008;
+        public static final int checkBox2=0x7f090009;
+        public static final int checkBox3=0x7f09000b;
+        public static final int checkBox4=0x7f09000c;
+        public static final int checkBox5=0x7f09000d;
+        public static final int editText1=0x7f09000e;
+        public static final int textView1=0x7f090002;
+        public static final int textView2=0x7f090003;
+        public static final int textView3=0x7f090004;
+        public static final int textView4=0x7f090005;
+        public static final int textView5=0x7f090006;
+        public static final int textView6=0x7f090007;
+        public static final int textView7=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -45,9 +56,16 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int bikes_allowed=0x7f06000b;
+        public static final int dog_friendly=0x7f060008;
+        public static final int geological=0x7f06000a;
         public static final int hello_world=0x7f060002;
+        public static final int historical=0x7f060009;
+        public static final int location=0x7f060005;
         public static final int nw=0x7f060003;
+        public static final int ocean_view=0x7f060006;
         public static final int profile=0x7f060004;
+        public static final int waterfall=0x7f060007;
     }
     public static final class style {
         /** 
