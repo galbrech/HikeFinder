7acdcbcdb98d3fd4416b366294eaedbbd2d3e64d
HoopRockStar <jlparris@ucsc.edu>
2014-02-17 13:14:35 -0800

    added button strings

--- gen/com/example/hikefinder/R.java
+++ gen/com/example/hikefinder/R.java
@@ -29,12 +29,11 @@ public final class R {
         public static final int yosemite=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int action_settings=0x7f090004;
         public static final int button1=0x7f090000;
-        public static final int button2=0x7f090002;
-        public static final int checkBox1=0x7f090003;
-        public static final int checkedTextView1=0x7f090004;
-        public static final int textView1=0x7f090001;
+        public static final int button2=0x7f090001;
+        public static final int checkBox1=0x7f090002;
+        public static final int checkedTextView1=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,6 +46,8 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int nw=0x7f060003;
+        public static final int profile=0x7f060004;
     }
     public static final class style {
         /** 
