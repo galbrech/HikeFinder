17527708f19a4ffd76ffd4048e0daf9ce20858d1
HoopRockStar <jlparris@ucsc.edu>
2014-02-17 22:22:14 -0800

    finished the hike_description.xml page and updated the main page

--- gen/com/example/hikefinder/R.java
+++ gen/com/example/hikefinder/R.java
@@ -26,35 +26,38 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int yosemite=0x7f020001;
+        public static final int map=0x7f020001;
+        public static final int yosemite=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090014;
+        public static final int action_settings=0x7f090015;
         public static final int button1=0x7f090000;
         public static final int button2=0x7f090001;
-        public static final int button3=0x7f090013;
-        public static final int checkBox1=0x7f09000e;
-        public static final int checkBox2=0x7f09000a;
-        public static final int checkBox3=0x7f090009;
-        public static final int checkBox4=0x7f09000b;
-        public static final int checkBox5=0x7f09000c;
-        public static final int editText1=0x7f09000d;
-        public static final int editText2=0x7f090010;
-        public static final int editText3=0x7f090012;
+        public static final int button3=0x7f090014;
+        public static final int checkBox1=0x7f09000f;
+        public static final int checkBox2=0x7f09000b;
+        public static final int checkBox3=0x7f09000a;
+        public static final int checkBox4=0x7f09000c;
+        public static final int checkBox5=0x7f09000d;
+        public static final int editText1=0x7f09000e;
+        public static final int editText2=0x7f090011;
+        public static final int editText3=0x7f090013;
+        public static final int imageButton1=0x7f090005;
         public static final int textView1=0x7f090002;
         public static final int textView2=0x7f090003;
         public static final int textView3=0x7f090004;
-        public static final int textView4=0x7f090005;
-        public static final int textView5=0x7f090006;
-        public static final int textView6=0x7f090007;
-        public static final int textView7=0x7f090008;
-        public static final int textView8=0x7f090011;
-        public static final int textView9=0x7f09000f;
+        public static final int textView4=0x7f090006;
+        public static final int textView5=0x7f090007;
+        public static final int textView6=0x7f090008;
+        public static final int textView7=0x7f090009;
+        public static final int textView8=0x7f090012;
+        public static final int textView9=0x7f090010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int options=0x7f030001;
-        public static final int route=0x7f030002;
+        public static final int hike_description=0x7f030001;
+        public static final int options=0x7f030002;
+        public static final int route=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
