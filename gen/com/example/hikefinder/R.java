8fec72238ce4c998d90a2206349f2385e3211746
bradt <bradt@ucsc.edu>
2014-02-18 15:03:32 -0800

    Added profile and results layout placeholders.

--- gen/com/example/hikefinder/R.java
+++ gen/com/example/hikefinder/R.java
@@ -28,16 +28,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int action_settings=0x7f090006;
         public static final int button1=0x7f090000;
         public static final int button2=0x7f090002;
         public static final int checkBox1=0x7f090003;
         public static final int checkedTextView1=0x7f090004;
+        public static final int retrn=0x7f090005;
         public static final int textView1=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int options=0x7f030001;
+        public static final int profile=0x7f030002;
+        public static final int results=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -46,6 +49,9 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int profile=0x7f060003;
+        public static final int results=0x7f060004;
+        public static final int retrn=0x7f060005;
     }
     public static final class style {
         /** 
