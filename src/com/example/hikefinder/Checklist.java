5bc58e14e173a624e5b4d922edd872146acafcf1
galbrech <galbrech@ucsc.edu>
2014-02-27 22:40:48 -0800

    added checklist and completed
    added checklist and completed xml and java parts.

--- src/com/example/hikefinder/Checklist.java
+++ src/com/example/hikefinder/Checklist.java
@@ -1,6 +1,5 @@
 package com.example.hikefinder;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,29 +7,29 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Profile extends Activity{
-	Button homeButton;
+public class Checklist  extends Activity{
+	Button backButton;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
+		
 		// when the screen is first created, show the layout
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.profile);
+		setContentView(R.layout.checklist);
 		Intent intent = getIntent();
 		//String value = intent.getStringExtra("key"); //if it's a string you stored.
+		backButton = (Button) findViewById(R.id.button1);
 		
-		homeButton = (Button) findViewById(R.id.retrn);
-		
-		homeButton.setOnClickListener( new OnClickListener() {
+		backButton.setOnClickListener( new OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-	        	Intent myIntent = new Intent(Profile.this, Completed.class);
+	        	Intent myIntent = new Intent(Checklist.this, Results.class);
 	        	//myIntent.putExtra("key", value); //Optional parameters
-	        	Profile.this.startActivity(myIntent);
+	        	Checklist.this.startActivity(myIntent);
             }
         });
+		
 	}
 
 }
